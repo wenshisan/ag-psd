5e720713448de348f0fe3b3668aa2ec14f48d3a9
wenshisan <519768628@qq.com>
2023-11-23 16:33:18 +0800

    测试增加按文件夹读取所有文件方法

--- src/test/tests.ts
+++ src/test/tests.ts
@@ -1,4 +1,4 @@
-import path = require('path');
+// import path = require('path');
 import fs = require('fs')
 import { ReadOptions } from '../psd';
 
@@ -6,8 +6,8 @@ import {
 	readPsdFromFile} from './common';
 
 
-const testFilesPath = path.join(__dirname, '..', '..', 'test');
-const readFilesPath = path.join(testFilesPath, 'read');
+// const testFilesPath = path.join(__dirname, '..', '..', 'test');
+// const readFilesPath = path.join(testFilesPath, 'read');
 // const readWriteFilesPath = path.join(testFilesPath, 'read-write');
 // const resultsFilesPath = path.join(__dirname, '..', '..', 'results');
 const opts: ReadOptions = {
@@ -15,15 +15,79 @@ const opts: ReadOptions = {
 	logMissingFeatures: true,
 };
 
-const blendModePSDPath = path.join(readFilesPath, 'blend-mode', 'src.psd')
+// const blendModePSDPath = path.join(readFilesPath, 'blend-mode', 'src.psd')
+
+// // @ts-ignore
+// const psd = readPsdFromFile(blendModePSDPath, { ...opts });
+
+const filePath = 'C:\\Users\\Jackson\\OneDrive\\shhz\\ps试题源文件\\ps资料\\ps试题源文件'
+
+const files = getFiles(filePath)
+
+for (let index = 0; index < files.length; index++) {
+	const fileActualPath = files[index];
+	// const fileActualPath = path.join(filePath , fileName)
+	const psdFile = readPsdFromFile(fileActualPath, { ...opts });
+	const dataFIlePath = fileActualPath + '.data.json'
+	delete psdFile.canvas
+	psdFile.children?.forEach(x => {
+		delete x.canvas
+		delete x.mask?.canvas
+		delete x.engineData
+	})
+
+	delete psdFile.imageResources
+	
+	delete psdFile.engineData
+	const jsonData = JSON.stringify(psdFile,null,2)
+
+	
+	fs.writeFileSync(dataFIlePath,jsonData)
+}
+
+// const filterFXPSDPath = path.join(readFilesPath, 'filterFX', 'src.psd')
+
+// // @ts-ignore
+// const filerFXPSD = readPsdFromFile(filterFXPSDPath, { ...opts });
+
+console.log('finished')
+
+
+
 
 // @ts-ignore
-const psd = readPsdFromFile(blendModePSDPath, { ...opts });
+function getFiles(dir: string, files: string[] = []) {
+	// Get an array of all files and directories in the passed directory using fs.readdirSync
+	const fileList = fs.readdirSync(dir)
+	// Create the full path of the file/directory by concatenating the passed directory and file/directory name
+	for (const file of fileList) {
+	  const name = `${dir}/${file}`
+	  // Check if the current file/directory is a directory using fs.statSync
+	  if (fs.statSync(name).isDirectory()) {
+		// If it is a directory, recursively call the getFiles function with the directory path and the files array
+		getFiles(name, files)
+	  } else {
+		// If it is a file, push the full path to the files array
+		files.push(name)
+	  }
+	}
+	return files
+}
 
-const filterFXPSDPath = path.join(readFilesPath, 'filterFX', 'src.psd')
-
-// @ts-ignore
-const filerFXPSD = readPsdFromFile(filterFXPSDPath, { ...opts });
-
-
-
+// function clearEmptyCanvasFields(layer: Psd | undefined | Layer) {
+// 	if (layer) {
+// 		if ('canvas' in layer && !layer.canvas) delete layer.canvas;
+// 		if ('imageData' in layer && !layer.imageData) delete layer.imageData;
+// 		layer.children?.forEach(clearEmptyCanvasFields);
+// 	}
+// }
+  
+// function clearCanvasFields(layer: Layer | undefined) {
+// 	if (layer) {
+// 		delete layer.canvas;
+// 		delete layer.imageData;
+// 		if (layer.mask) delete layer.mask.canvas;
+// 		if (layer.mask) delete layer.mask.imageData;
+// 		layer.children?.forEach(clearCanvasFields);
+// 	}
+// }
